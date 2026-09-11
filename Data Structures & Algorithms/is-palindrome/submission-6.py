2ec54b3a70851bc745a64e068cc16056895882b6
yasseroufqir <yassoufqir@gmail.com>
2026-09-11 10:45:30 +0200

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l , r = 0 , len(s)-1
+        while l < r:
+            if not s[l].isalnum():
+                l+=1
+                continue
+            elif not s[r].isalnum():
+                r-=1
+                continue
+            elif s[l].isalnum() and s[r].isalnum() and s[l].lower() == s[r].lower():
+                l+=1
+                r-=1
+                
+            else:
+                return False
+        return True
